2cd71e5e54aad58027891a972de3aad080c02617
cdsar626 <cdsar626@gmail.com>
2025-06-27 18:28:50 -0400

    Upgraded Gradle and Dependencies successfully

--- android/app/src/main/kotlin/com/cdsar626/clean_bucket_list/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cdsar626.clean_bucket_list
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/cdsar626/tablist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cdsar626.tablist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
